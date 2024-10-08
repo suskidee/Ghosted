03920a51f66867620e4bab9d6e8203a373862322
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-10-08 12:33:02 +0100

    started docker with mysql database

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -5,8 +5,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('products', views.ApiProducts, basename='product')
 
-
-
 urlpatterns = [
     path("", include(router.urls)),
     path("search/", views.ProductSearchView.as_view(), name='product-search')
